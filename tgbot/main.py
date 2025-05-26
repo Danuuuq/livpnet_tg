ade8f9cb6100488c31e5c9593ff1311995c580ba
Daniil Tyunkov <daniil@tyunkov.ru>
2025-05-26 12:53:52 +0300

    ready structure

--- tgbot/main.py
+++ tgbot/main.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from aiohttp import web
 from aiogram.webhook.aiohttp_server import (
     SimpleRequestHandler,
@@ -15,40 +13,31 @@ from app.core.logger import logger # noqa
 async def on_startup() -> None:
     """Действия при запуске бота."""
     await set_commands()
-    await bot.set_webhook(settings.get_webhook_url)
-    await bot.send_message(chat_id=settings.ADMINS, text='Бот запущен')
+    await bot.set_webhook(settings.get_webhook_url,
+                          secret_token=settings.WEBHOOK_SECRET)
 
 
 async def on_shutdown() -> None:
     """Действия при остановке бота."""
-    await bot.send_message(chat_id=settings.ADMINS, text='Бот остановлен!')
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.session.close()
 
 
-def main():
+async def main():
     """Запуск приложения с ботом."""
 
-    if settings.WEBHOOK_MODE:
-        dp.startup.register(on_startup)
-        dp.shutdown.register(on_shutdown)
-        app = web.Application()
-        webhook_requests_handler = SimpleRequestHandler(
-            dispatcher=dp,
-            bot=bot
-        )
-        webhook_requests_handler.register(app, path=settings.WEBHOOK_PATH)
-        setup_application(app, dp, bot=bot)
-        web.run_app(app, host=settings.APP_HOST, port=settings.APP_PORT)
-    # else:
-    #     try:
-    #         await bot.delete_webhook(drop_pending_updates=True)
-    #         await dp.start_polling(
-    #             bot, allowed_updates=dp.resolve_used_update_types())
-    #     finally:
-    #         await bot.session.close()
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=settings.WEBHOOK_SECRET,
+    )
+    webhook_requests_handler.register(app, path=settings.WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host=settings.APP_HOST, port=settings.APP_PORT)
 
 
 if __name__ == '__main__':
-    # asyncio.run(main())
     main()
