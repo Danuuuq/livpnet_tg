5f3a2189588d7e0ae3b4c88eb33b8b4a3f4db1a2
Daniil Tyunkov <daniil@tyunkov.ru>
2025-07-07 15:27:09 +0300

    add log for middleware

--- backend/app/middlewares/ip_access.py
+++ backend/app/middlewares/ip_access.py
@@ -3,6 +3,7 @@ from fastapi import Request, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.core.config import settings
+from app.core.log_config import log_action_status
 
 allowed_networks = [
     ipaddress.ip_network(ip) for ip in settings.ALLOWED_IP_YOOKASSA
@@ -16,6 +17,10 @@ class IPWhitelistMiddleware(BaseHTTPMiddleware):
 
         if request.url.path.startswith('/subscription/yookassa'):
             if not any(ip_obj in network for network in allowed_networks):
+                log_action_status(
+                    action_name='Обращение к /subscription/yookassa',
+                    message=f'Заблокирован доступ с ip-адреса: {ip_obj}, '
+                )
                 raise HTTPException(status_code=403, detail='Access denied')
 
         return await call_next(request)
